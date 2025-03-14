385c9a0162aac61302eff090c01b93f5b79cc5b7
gokul-MCA <154983453+gokul-MCA@users.noreply.github.com>
2025-03-14 23:29:00 +0530

    fix: "added _redirects"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./components/home/Home"
-import Header from "./components/header/Header"
-import Events from "./components/events-listing/Events"
-import Footer from "./components/footer/Footer"
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Events from "./components/events-listing/Events";
+import Footer from "./components/footer/Footer";
+import NoFound from "./components/no-found/NoFound";
 
 function App() {
- 
-
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events-listing" element={<Events/>} />
-    </Routes>
-    <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events-listing" element={<Events />} />
+        <Route path="*" element={<NoFound />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/no-found/NoFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoFound = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>NoFound</div>
+  )
+}
+
+export default NoFound
